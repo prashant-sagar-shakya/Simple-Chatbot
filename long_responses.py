643c23d2e419f51f14ca41ce74b044ca7f9a9652
Prashant Sagar Shakya <therealprashantshakya@gmail.com>
2024-04-08 16:34:39 +0530

    Simple Chatbot using Python

---
+++ long_responses.py
@@ -0,0 +1,11 @@
+import random
+
+R_EATING = "I don't like eating anything because I'm a bot obviously!"
+
+
+def unknown():
+    response=['Could you please re-pharase that?',
+              "...",
+              "Sounds about right",
+              "What does that mean?"][random.randrange(4)]
+    return response
